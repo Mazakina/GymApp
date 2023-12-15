784e0ed7a48417eb8ed46bd0f9a04180d1daa915
Paulo Mazakina <8.mazakina@gmail.com>
2023-12-14 22:24:09 -0300

    feat: add CI workflow

--- src/http/controllers/check-ins/validate.spec.ts
+++ src/http/controllers/check-ins/validate.spec.ts
@@ -14,7 +14,7 @@ describe('Validate Check-in (e2e)', () => {
   })
 
   it('should be able to get validate a check-in', async () => {
-    const { token } = await createAndAuthenticateUser(app)
+    const { token } = await createAndAuthenticateUser(app, true)
 
     const gym = await prisma.gym.create({
       data: {
